5ebac957f28cf6ef412d4d1e3924c7803cde66c2
Kalundaah <84059634+kalundaah@users.noreply.github.com>
2024-08-08 20:39:00 +0300

    Add date and uuid for id column. Overrode the __init__

--- models/note.py
+++ models/note.py
@@ -1,16 +1,25 @@
 """
 The note model. Contains a title,content for now.
 """
+from datetime import datetime
+
 from server import db
+import uuid
 
 
 class Note(db.Model):
     """
     A note model. Contains a title,content for now.
     """
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(36), primary_key=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     title = db.Column(db.String(80), unique=True, nullable=False)
     content = db.Column(db.Text, nullable=True)
 
-
-
+    def __init__(self, title, content=None):
+        self.id = str(uuid.uuid4())
+        self.title = title
+        self.content = content
+        self.created_at = datetime.utcnow()
+        self.updated_at = self.created_at
