dc630bb5c9add5a14436efa53d92fe0646e3255d
Kalundaah <84059634+kalundaah@users.noreply.github.com>
2024-08-08 21:29:55 +0300

    Delete Note

--- server/views/note/__init__.py
+++ server/views/note/__init__.py
@@ -7,5 +7,5 @@ note = Blueprint('note', __name__)
 
 from server.views.note.create_note import create_note
 from server.views.note.view_note import view_notes
-
+from server.views.note.delete_note import delete_note
 
---
+++ server/views/note/delete_note.py
@@ -0,0 +1,24 @@
+"""
+Delete note
+"""
+# dependencies
+from flask import request
+# blueprint
+from . import note
+# db
+from server import db
+# Notes model
+from models.note import Note
+
+
+@note.route('/delete_note/<string:id_nos>', methods=['DELETE'])
+def delete_note(id_nos):
+    del_note = db.get_or_404(Note, id_nos)
+    # print(type(del_note))
+    hold = del_note.__dict__
+    try:
+        db.session.delete(del_note)
+        db.session.commit()
+    except Exception as e:
+        print(str(e))
+    return f"The note {hold['title']} has been deleted.", 200
